5ef7f750e5b2dc30f66b4d466fdb96457821683c
Matt Hackett <richtaur@gmail.com>
2011-09-15 15:47:40 -0700

    yay css3 animations

--- htdocs/fetch/index.php
+++ htdocs/fetch/index.php
@@ -1,7 +1,5 @@
 <?php
 
-error_log('--- begin new request ---');
-
 define('URL_INFO', 'http://gatherer.wizards.com/Handlers/InlineCardSearch.ashx?nameFragment=%s&cacheBust=%s');
 define('URL_IMAGE', 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=%s&type=card');
 
@@ -15,9 +13,7 @@ if (!$card_name) {
 
 // Fetch the card ID
 $url = sprintf(URL_INFO, $card_name, time());
-error_log($url);
 $data = file_get_contents($url);
-error_log($data);
 $json = json_decode($data);
 
 if (count($json->Results) === 0) {
@@ -27,15 +23,12 @@ if (count($json->Results) === 0) {
 
 $result = $json->Results[0];
 $id = $result->ID;
-error_log($id);
 
 // Use the ID to find the image URL
 $image_url = sprintf(URL_IMAGE, $id);
 
 // Output our data
 header('Content-type: application/json');
-error_log($result->Name);
-error_log($image_url);
 echo json_encode((object) array(
 	'id' => $id,
 	'name' => $result->Name,
